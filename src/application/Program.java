3524a5a43c8cf89332253da8f10786a20e11ac7f
geneciSI <genecicouto@id.uff.br>
2025-01-30 23:07:38 -0300

    Exercicio com 2 interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,27 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
+	
 	public static void main(String[] args) {
-		Printer p = new Printer("1080");
+		
+		ConcretePrinter p = new ConcretePrinter("1080");
 		p.processDoc("My Letter");
 		p.print("My Letter");
+		System.out.println();
 		
-		Scanner s = new Scanner("2003");
+		ConcreteScanner s = new ConcreteScanner("2003");
 		s.processDoc("My Email");
 		System.out.println("Scan result: " + s.scan());
+		System.out.println();
+		
+		ComboDevice c = new ComboDevice("2081");
+		c.processDoc("My Dissertation");
+		c.print("My Dissertation");
+		System.out.println("Scan result: " + c.scan());
+		
 	}
 }
